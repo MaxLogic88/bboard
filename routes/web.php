852b73987bc8f3080317b1609cc8364848d74935
Max Logic <neur0z@yandex.ru>
2023-02-07 19:12:29 +0300

    Added autorized policy for Bb model

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,12 @@ Auth::routes();
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/home/add', [HomeController::class, 'showAddBbForm'])->name('bb.add');
 Route::post('/home', [HomeController::class, 'storeBb'])->name('bb.store');
-Route::get('/home/{bb}/edit', [HomeController::class, 'showEditBbForm'])->name('bb.edit');
-Route::patch('/home/{bb}', [HomeController::class, 'updateBb'])->name('bb.update');
-Route::get('/home/{bb}/delete', [HomeController::class, 'showDeleteBbForm'])->name('bb.delete');
-Route::delete('/home/{bb}', [HomeController::class, 'destroyBb'])->name('bb.destroy');
+//В Middleware посредник can реализует разграничение доступа к моделям посредством связанных с ними политик
+//Посредник can принимает 2 параметра, указанных через запятую:
+//--Название выполнямой операции, совподающее с именем метода в политике
+//--Имя URL параметра, содержащего номер записи
+Route::get('/home/{bb}/edit', [HomeController::class, 'showEditBbForm'])->name('bb.edit')->middleware('can:update,bb');
+Route::patch('/home/{bb}', [HomeController::class, 'updateBb'])->name('bb.update')->middleware('can:update,bb');
+Route::get('/home/{bb}/delete', [HomeController::class, 'showDeleteBbForm'])->name('bb.delete')->middleware('can:destroy,bb');
+Route::delete('/home/{bb}', [HomeController::class, 'destroyBb'])->name('bb.destroy')->middleware('can:destroy,bb');
 Route::get('/{bb}', [BbsController::class, 'detail'])->name('detail');
